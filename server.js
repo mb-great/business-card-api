347029216c4ec06a597a0160317672506e994e89
mb-great <mokshbudhiraja@gmail.com>
2024-07-23 01:55:47 +0530

    fixed routes

--- server.js
+++ server.js
@@ -4,6 +4,11 @@ const cardRoutes = require('./routes');
 const app = express();
 const port = 3000;
 
+// Route to handle the root URL
+app.get('/', (req, res) => {
+  res.send('Welcome to the Business Card API. Use /business-card to generate a business card.');
+});
+
 app.use('/business-card', cardRoutes);
 
 app.listen(port, () => {
